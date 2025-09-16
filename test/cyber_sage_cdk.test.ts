393a752dc9cc461444be5485c6d4e371068f5550
JarredTD <87108293+JarredTD@users.noreply.github.com>
2025-09-15 17:52:36 -0700

    Initial commit

---
+++ test/cyber_sage_cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CyberSageCdk from '../lib/cyber_sage_cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cyber_sage_cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CyberSageCdk.CyberSageCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
